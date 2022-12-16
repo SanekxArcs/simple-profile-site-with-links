1c324d6a87b7f81f4204c1a3054cd774546f294a
Oleksandr Dzisiak <shumahersasha007@gmail.com>
2022-12-16 23:37:05 +0100

    update

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ const english = document.getElementById("en"),
     change_text = document.getElementById("translate"),
     r = document.querySelector(":root"),
     body = document.querySelector("body"),
-    buttonChangeTheme = document.querySelector(".theme");
+    buttonChangeTheme = document.querySelector(".theme"),
+    cvLinkChangeEN = document.querySelector('.ico-cv_en'),
+    cvLinkChangePL = document.querySelector('.ico-cv_pl'),
+    cvLinkChangeUA = document.querySelector('.ico-cv_ua');
 
 window.addEventListener("blur", () => {
     document.title = langTitle;
@@ -45,29 +48,48 @@ function change(lang_on, lang_off1, lang_off2) {
         lang_off1.classList.remove("current_lang");
         lang_off2.classList.remove("current_lang");
     }
+        cvLinkChangeEN.classList.remove('hide');
+        cvLinkChangeEN.classList.remove('show');
+        cvLinkChangePL.classList.remove('hide');
+        cvLinkChangePL.classList.remove('show');
+        cvLinkChangeUA.classList.remove('hide');
+        cvLinkChangeUA.classList.remove('show');
+    
 
     if (lang_on.innerHTML == "EN") {
         change_text.innerHTML =
-            "Hi, I'm a web developer from Warsaw, PL. Currently working on freelance projects. In my spare time I enjoy DIY, gaming and building my own projects. My preferred tools are vanilla JS, React and HTML/CSS. Good at communicating with clients.";
+            "Hi, I'm a web developer from Warsaw, PL.<br>Currently working on freelance projects.<br>In my spare time I enjoy DIY, gaming and building my own projects.<br>My preferred tools are vanilla JS, React and HTML/CSS.<br>Good at communicating with clients.";
         change_name.innerHTML = "Oleksandr Dzisiak";
         change_work.innerHTML = "Front-end Developer";
         langTitle = "Come back👏🏻";
         r.style.setProperty("--Flag", "var(--EN)");
+        cvLinkChangeEN.classList.add('show');
+        cvLinkChangePL.classList.add('hide');
+        cvLinkChangeUA.classList.add('hide');
+
+        
 
     } else if (lang_on.innerHTML == "PL") {
         change_text.innerHTML =
-            "Cześć, jestem web developerem z Warszawy. Obecnie pracuje nad niezależnymi projektami. W wolnym czasie lubię majsterkować, grać i tworzyć własne projekty. Moje preferowane narzędzia to Vanilla JS, React i HTML/CSS. Dobry w komunikacji z klientami.";
+            "Cześć, jestem web developerem z Warszawy. Obecnie pracuje nad niezależnymi projektami. W wolnym czasie lubię majsterkować, grać i tworzyć własne projekty.<br>Moje preferowane narzędzia to Vanilla JS, React i HTML/CSS.<br>Dobry w komunikacji z klientami.";
         change_name.innerHTML = "Oleksandr Dzisiak";
         change_work.innerHTML = "Front-end Developer";
         langTitle = "Wróć👏🏻";
         r.style.setProperty("--Flag", "var(--PL)");
 
+        cvLinkChangeEN.classList.add('hide');
+        cvLinkChangePL.classList.add('show');
+        cvLinkChangeUA.classList.add('hide');
+
     } else if (lang_on.innerHTML == "UA") {
-        change_text.innerHTML = `Привіт, я веб-розробник із Варшави. Зараз працюю над фрілансерськими проектами. У вільний час я люблю DIY, ігри та створення власних проектів. Моїми улюбленими інструментами є ванільний JS, React і HTML/CSS. Добре вмію спілкуватися з клієнтами.`;
+        change_text.innerHTML = `Привіт, я веб-розробник із Варшави.<br>Зараз працюю над фрілансерськими проектами.<br>У вільний час я люблю DIY, ігри та створення власних проектів.<br>Моїми улюбленими інструментами є ванільний JS, React і HTML/CSS.<br>Добре вмію спілкуватися з клієнтами.`;
         change_name.innerHTML = "Олександр Дзісяк";
         change_work.innerHTML = "Фронтенд розробник";
         langTitle = "Повернись👏🏻";
         r.style.setProperty("--Flag", "var(--UA)");
+        cvLinkChangeEN.classList.add('hide');
+        cvLinkChangePL.classList.add('hide');
+        cvLinkChangeUA.classList.add('show');
     }
 }
 
